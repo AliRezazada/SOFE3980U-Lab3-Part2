9701b8dcf35735638f5580266a26446ea62cf8ad
AliRezazada <139565382+AliRezazada@users.noreply.github.com>
2025-03-03 19:40:51 -0500

    Update Binary.java

--- BinaryCalculatorWebapp/src/main/java/com.ontariotechu.sofe3980U/Binary.java
+++ BinaryCalculatorWebapp/src/main/java/com.ontariotechu.sofe3980U/Binary.java
@@ -2,80 +2,46 @@ package com.ontariotechu.sofe3980U;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
-public class BinaryAPIController {
+@Controller
+public class BinaryController {
 
-	@GetMapping("/add")
-	public String addString(@RequestParam(name="operand1", required=false, defaultValue="") String operand1,
-                       @RequestParam(name="operand2", required=false, defaultValue="") String operand2) {
-		Binary number1=new Binary (operand1);
-		Binary number2=new Binary (operand2);
-        return  Binary.add(number1,number2).getValue();
-		// http://localhost:8080/add?operand1=111&operand2=1010
-	}
-	
-	@GetMapping("/add_json")
-	public BinaryAPIResult addJSON(@RequestParam(name="operand1", required=false, defaultValue="") String operand1,
-                       @RequestParam(name="operand2", required=false, defaultValue="") String operand2) {
-		Binary number1=new Binary (operand1);
-		Binary number2=new Binary (operand2);
-        return  new BinaryAPIResult(number1,"add",number2,Binary.add(number1,number2));
-		// http://localhost:8080/add?operand1=111&operand2=1010
+	@GetMapping("/")
+	public String getCalculator(@RequestParam(name = "operand1", required = false, defaultValue = "") String operand1,
+			Model model) {
+		model.addAttribute("operand1", operand1);
+		model.addAttribute("operand1Focused", operand1.length() > 0);
+		return "calculator";
 	}
 
-	@GetMapping("/or")
-	public String orString(@RequestParam(name="operand1", required=false, defaultValue="") String operand1,
-                       @RequestParam(name="operand2", required=false, defaultValue="") String operand2) {
-		Binary number1=new Binary (operand1);
-		Binary number2=new Binary (operand2);
-        return  Binary.orOperation(number1,number2).getValue();
-		// http://localhost:8080/add?operand1=111&operand2=1010
-	}
-	@GetMapping("/or_json")
-	public BinaryAPIResult orJSON(@RequestParam(name="operand1", required=false, defaultValue="") String operand1,
-                       @RequestParam(name="operand2", required=false, defaultValue="") String operand2) {
-		Binary number1=new Binary (operand1);
-		Binary number2=new Binary (operand2);
-        return  new BinaryAPIResult(number1,"or",number2,Binary.orOperation(number1,number2));
-		// http://localhost:8080/add?operand1=111&operand2=1010
-	}
-
-	@GetMapping("/and")
-	public String andString(@RequestParam(name="operand1", required=false, defaultValue="") String operand1,
-                       @RequestParam(name="operand2", required=false, defaultValue="") String operand2) {
-		Binary number1=new Binary (operand1);
-		Binary number2=new Binary (operand2);
-        return  Binary.andOperation(number1,number2).getValue();
-		// http://localhost:8080/add?operand1=111&operand2=1010
-	}
-	@GetMapping("/and_json")
-	public BinaryAPIResult andJSON(@RequestParam(name="operand1", required=false, defaultValue="") String operand1,
-                       @RequestParam(name="operand2", required=false, defaultValue="") String operand2) {
-		Binary number1=new Binary (operand1);
-		Binary number2=new Binary (operand2);
-        return  new BinaryAPIResult(number1,"and",number2,Binary.andOperation(number1,number2));
-		// http://localhost:8080/add?operand1=111&operand2=1010
-	}
-
-	@GetMapping("/multiply")
-	public String mulString(@RequestParam(name="operand1", required=false, defaultValue="") String operand1,
-                       @RequestParam(name="operand2", required=false, defaultValue="") String operand2) {
-		Binary number1=new Binary (operand1);
-		Binary number2=new Binary (operand2);
-        return  Binary.multiplyBinary(number1,number2).getValue();
-		// http://localhost:8080/add?operand1=111&operand2=1010
-	}
-	@GetMapping("/mul_json")
-	public BinaryAPIResult mulJSON(@RequestParam(name="operand1", required=false, defaultValue="") String operand1,
-                       @RequestParam(name="operand2", required=false, defaultValue="") String operand2) {
-		Binary number1=new Binary (operand1);
-		Binary number2=new Binary (operand2);
-        return  new BinaryAPIResult(number1,"multiply",number2,Binary.multiplyBinary(number1,number2));
-		// http://localhost:8080/add?operand1=111&operand2=1010
+	@PostMapping("/")
+	public String result(@RequestParam(name = "operand1", required = false, defaultValue = "") String operand1,
+			@RequestParam(name = "operator", required = false, defaultValue = "") String operator,
+			@RequestParam(name = "operand2", required = false, defaultValue = "") String operand2, Model model) {
+		model.addAttribute("operand1", operand1);
+		model.addAttribute("operator", operator);
+		model.addAttribute("operand2", operand2);
+		Binary number1 = new Binary(operand1);
+		Binary number2 = new Binary(operand2);
+		switch (operator) {
+			case "+":
+				model.addAttribute("result", Binary.add(number1, number2).getValue());
+				return "result";
+			case "|":
+				model.addAttribute("result", Binary.orOperation(number1, number2).getValue());
+				return "result";
+			case "&":
+				model.addAttribute("result", Binary.andOperation(number1, number2).getValue());
+				return "result";
+			case "*":
+				model.addAttribute("result", Binary.multiplyBinary(number1, number2).getValue());
+				return "result";
+			default:
+				return "Error";
+		}
 	}
 
 }
